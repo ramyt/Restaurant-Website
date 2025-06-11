f9cd0db7565461f3a54f411dbc7919cdcb0ddb90
ramyt <ramyt@users.noreply.github.com>
2025-06-11 16:57:26 +0700

    Update models.py

--- src/menu/models.py
+++ src/menu/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.text import slugify
 
 
